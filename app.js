1a4133b6d23421a0e07a415f2e1a32e023d4dd12
kb4ek <kbaek1202@gmail.com>
2019-05-08 21:43:13 +0900

    CRUD-easyAPI(2)

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const User = require('./models').User;
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extend: false}));
+app.use(bodyParser.urlencoded({
+    extend: false
+}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 sequelize.sync();
@@ -19,78 +21,108 @@ app.listen(PORT, () => {
 })
 
 app.get('/', async (req, res) => {
-    try{
+    try {
         var userID = '';
         var userPW = '';
         var userName = '';
-        
-        await User.findOne({where : {id: 'admin1'}}).then(user => {
-            if(!user){
+
+        await User.findOne({
+            where: {
+                id: 'admin1'
+            }
+        }).then(user => {
+            if (!user) {
                 return res.render('index');
             } else {
                 var userID = user.id;
                 var userPW = user.pw;
                 var userName = user.name;
 
-                return res.render('index', {id: userID, pw: userPW, name: userName});
+                return res.render('index', {
+                    id: userID,
+                    pw: userPW,
+                    name: userName
+                });
             }
         })
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         return res.send("<script>history.back();</script>");
     }
 })
 
 app.post('/join', async (req, res) => {
-    try{
-        await User.findOne({where : {id: req.body.id}}).then(user => {
-            if(!user){
+    try {
+        await User.findOne({
+            where: {
+                id: req.body.id
+            }
+        }).then(user => {
+            if (!user) {
                 User.create({
                     id: req.body.id,
                     pw: req.body.pw,
                     name: req.body.name
                 });
-            
+
                 return res.send("<script>alert('회원가입 성공');location.href='/'</script>");
             } else {
                 return res.send("<script>alert('오류');location.href='/'</script>");
             }
         })
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         return res.send("<script>location.href='/'</script>");
     }
 })
 
 app.post('/update', async (req, res) => {
-    try{
-        await User.findOne({where : {id: req.body.nowID}}).then(user => {
-            if(!user) {
+    try {
+        await User.findOne({
+            where: {
+                id: req.body.nowID
+            }
+        }).then(user => {
+            if (!user) {
                 return res.send("<script>alert('오류');location.href='/'</script>");
             } else {
-                User.update({id: req.body.updateId}, {where: {id: req.body.nowID}});
+                User.update({
+                    id: req.body.updateId
+                }, {
+                    where: {
+                        id: req.body.nowID
+                    }
+                });
                 return res.send("<script>alert('성공');location.href='/'</script>");
             }
         })
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         return res.send("<script>location.href='/'</script>");
     }
 })
 
 app.post('/delete', async (req, res) => {
-    try{
-        await User.findOne({where: {id: req.body.deleteId}}).then(user => {
-            if(!user) {
+    try {
+        await User.findOne({
+            where: {
+                id: req.body.deleteId
+            }
+        }).then(user => {
+            if (!user) {
                 return res.send("<script>alert('오류');location.href='/'</script>");
-            } else if(user.id === req.body.deleteId && user.pw === req.body.deletePw && user.name === req.body.deleteName) {
-                User.destroy({where: {id: req.body.deleteId}});
+            } else if (user.id === req.body.deleteId && user.pw === req.body.deletePw && user.name === req.body.deleteName) {
+                User.destroy({
+                    where: {
+                        id: req.body.deleteId
+                    }
+                });
                 return res.send("<script>alert('성공');location.href='/'</script>");
             } else {
                 return res.send("<script>alert('오류1');location.href='/'</script>");
             }
         })
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         return res.send("<script>location.href='/'</script>");
     }
